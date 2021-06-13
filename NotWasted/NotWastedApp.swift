bf7b1c107869a09a65f623481bdae043ce92a568
Harsh Tagotra <36056869+hxt1965@users.noreply.github.com>
2021-06-13 08:43:17 +0530

    Initial Commit

---
+++ NotWasted/NotWastedApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotWastedApp.swift
+//  NotWasted
+//
+//  Created by admin on 13/06/21.
+//
+
+import SwiftUI
+
+@main
+struct NotWastedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
